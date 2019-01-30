77f5fbad2ee93e8a244b609b250d6dab46ec433d
Maria108 <maria.abash@gmail.com>
2019-01-29 22:50:48 -0500

    Testimonials

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -4,66 +4,23 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 import '../styles/testimonials.css'
-import { Grid } from 'react-bootstrap'
+import { Grid, Col, Row } from 'react-bootstrap'
 
 const Testimonials = () => (
   <Layout>
     <SEO title="testimonials" />
     <Grid align="center">
       <div class="customers-main">
-        <Grid>
-          <div className="top-review">
+        <Row>
+          <Col xs={12} md={2} />
+          <Col xs={12} md={8} className="top-review">
             <p>
               -This is amazing. Feel so much better. Lena is the best therapist
               in the world!-
             </p>
-            <h1 className="testimon">-Maria A-</h1>
-          </div>
-        </Grid>
-      </div>
-      <div class="customers-main">
-        <Grid>
-          <div className="custom-info">
-            <p>
-              This is amazing. Feel so much better. Lena is the best therapist
-              in the world!
-            </p>
-            <h1 className="testimon">-Park S-</h1>
-          </div>
-        </Grid>
-      </div>
-      <div class="customers-main">
-        <Grid>
-          <div className="custom-info">
-            <p>
-              This is amazing. Feel so much better. Lena is the best therapist
-              in the world!
-            </p>
-            <h1 className="testimon">-Park S-</h1>
-          </div>
-        </Grid>
-      </div>
-      <div class="customers-main">
-        <Grid>
-          <div className="custom-info">
-            <p>
-              This is amazing. Feel so much better. Lena is the best therapist
-              in the world!
-            </p>
-            <h1 className="testimon">-Park S-</h1>
-          </div>
-        </Grid>
-      </div>
-      <div class="customers-main">
-        <Grid>
-          <div className="custom-info">
-            <p>
-              This is amazing. Feel so much better. Lena is the best therapist
-              in the world!
-            </p>
-            <h1 className="testimon">-Park S-</h1>
-          </div>
-        </Grid>
+            <p className="testimon">-Maria A-</p>
+          </Col>
+        </Row>
       </div>
     </Grid>
   </Layout>
