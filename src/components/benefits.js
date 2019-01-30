75a4d1e34b0272851c904175649065a4e7ae1008
Maria108 <maria.abash@gmail.com>
2019-01-30 13:52:27 -0500

    Benefits

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -11,8 +11,17 @@ const Benefits = () => (
           <Col xs={12} md={6}>
             <div className="white-bckgrnd-about">
               <Row>
-                <Col xs={12} md={6} className="recommendations">
-                  <p>Something about colonics</p>
+                <Col xs={12} md={12} className="about-colonic">
+                  <p>
+                    Colonic is a thorough cleansing of the colon ( large
+                    intestine), using only warm purified water. During the
+                    treatment there is a continuous inflow and outflow of water
+                    that doesn’t allow pressure to build up, witch makes it for
+                    a comfortable and safe session. The therapist is with you
+                    during the whole session, doing firm abdominal massage to
+                    assure gentle and effective release. It usually takes 45
+                    mins to an hour to empty out a colon
+                  </p>
                 </Col>
               </Row>
             </div>
