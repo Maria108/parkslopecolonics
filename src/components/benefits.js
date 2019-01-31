346f124b76e545802d83d47817018dc31fa64259
Maria108 <maria.abash@gmail.com>
2019-01-31 09:26:55 -0500

    Benefits

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -12,8 +12,8 @@ const Benefits = () => (
             <div className="white-bckgrnd-about">
               <Row>
                 <Col xs={12} md={12} className="about-colonic">
-                  <p>
-                    Colonic is a thorough cleansing of the colon ( large
+                  <p className="what-is-colonic">
+                    Colonic is a <b>thorough cleansing</b> of the colon (large
                     intestine), using only warm purified water. During the
                     treatment there is a continuous inflow and outflow of water
                     that doesn’t allow pressure to build up, witch makes it for
