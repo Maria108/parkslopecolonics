311f74857cbe7a96cb9cde481b778fc70b4ba8b5
Maria108 <maria.abash@gmail.com>
2019-02-01 16:04:19 -0500

    Footer form

--- src/components/footer.js
+++ src/components/footer.js
@@ -26,7 +26,7 @@ const Footer = () => (
         </Col>
         <Col xs={6} md={6}>
           <div>
-            <form name="contact" netlify>
+            <form name="contact" method="POST" data-netlify="true">
               <input type="hidden" name="form-name" />
               <p className="message-me">
                 <label>
