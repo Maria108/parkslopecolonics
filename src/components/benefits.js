5d3a56428480e0ec6723df5bfaf176b4e1651c14
Maria108 <maria.abash@gmail.com>
2019-02-03 22:55:59 -0500

    First section about page

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -5,30 +5,23 @@ import { Row, Col, Grid } from 'react-bootstrap'
 
 const Benefits = () => (
   <Grid>
-    <div className="main-div-colon picture-about">
-      <div className="opacity-dive">
-        <Row>
-          <Col xs={12} md={6}>
-            <div className="white-bckgrnd-about">
-              <Row>
-                <Col xs={12} md={12} className="about-colonic">
-                  <p className="what-is-colonic">
-                    Colonic is a <b>thorough cleansing</b> of the colon (large
-                    intestine), using only warm purified water. During the
-                    treatment there is a continuous inflow and outflow of water
-                    that doesn’t allow pressure to build up, witch makes it for
-                    a comfortable and safe session. The therapist is with you
-                    during the whole session, doing firm abdominal massage to
-                    assure gentle and effective release. It usually takes 45
-                    mins to an hour to empty out a colon
-                  </p>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    </div>
+    <Row className="first-section-about">
+      <Col xs={12} md={6}>
+        <p className="how-you-know">GRAVITY METHOD COLONICS</p>
+        <p className="about-colonic">
+          Colonic is a <b>thorough cleansing</b> of the colon (large intestine),
+          using only warm purified water. During the treatment there is a
+          continuous inflow and outflow of water that doesn’t allow pressure to
+          build up, witch makes it for a comfortable and safe session. The
+          therapist is with you during the whole session, doing firm abdominal
+          massage to assure gentle and effective release. It usually takes 45
+          mins to an hour to empty out a colon
+        </p>
+      </Col>
+      <Col xs={12} md={6}>
+        <div className="main-div-colon picture-about" />
+      </Col>
+    </Row>
   </Grid>
 )
 
