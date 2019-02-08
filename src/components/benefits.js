69ee5bb725cf987c1993a92b47edd1e9a0f27c6b
Maria108 <maria.abash@gmail.com>
2019-02-07 22:43:36 -0500

    Benefits

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -4,25 +4,23 @@ import '../styles/about.css'
 import { Row, Col, Grid } from 'react-bootstrap'
 
 const Benefits = () => (
-  <Grid>
-    <Row className="first-section-about">
-      <Col xs={6} md={5}>
-        <p className="gravity-method-header">GRAVITY METHOD COLONICS</p>
-        <p className="about-colonic">
-          Colonic is a <b>thorough cleansing</b> of the colon (large intestine),
-          using only warm purified water. During the treatment there is a
-          continuous inflow and outflow of water that doesn’t allow pressure to
-          build up, which makes it for a comfortable and safe session. The
-          therapist is with you during the whole session, doing firm abdominal
-          massage to assure gentle and effective release. It usually takes 45
-          mins to an hour to empty out a colon
-        </p>
-      </Col>
-      <Col xs={6} md={7}>
-        <div className="main-div-colon-ananas picture-about" />
-      </Col>
-    </Row>
-  </Grid>
+  <Row className="first-section-about">
+    <Col xs={6} md={5}>
+      <p className="gravity-method-header">GRAVITY METHOD COLONICS</p>
+      <p className="about-colonic">
+        Colonic is a <b>thorough cleansing</b> of the colon (large intestine),
+        using only warm purified water. During the treatment there is a
+        continuous inflow and outflow of water that doesn’t allow pressure to
+        build up, which makes it for a comfortable and safe session. The
+        therapist is with you during the whole session, doing firm abdominal
+        massage to assure gentle and effective release. It usually takes 45 mins
+        to an hour to empty out a colon
+      </p>
+    </Col>
+    <Col xs={6} md={7}>
+      <div className="main-div-colon-ananas picture-about" />
+    </Col>
+  </Row>
 )
 
 export default Benefits
