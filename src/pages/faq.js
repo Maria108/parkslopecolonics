cb40d65477ec4d1addc10c77171e4dbe29642e14
Maria108 <maria.abash@gmail.com>
2019-02-24 18:58:47 -0500

    FAQ page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -4,6 +4,8 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Grid, Row, Col } from 'react-bootstrap'
 
+import { FaCircle } from 'react-icons/fa'
+
 import '../styles/faq.css'
 
 const Faq = () => (
@@ -11,10 +13,40 @@ const Faq = () => (
     <SEO title="faq" />
     <Grid>
       <Row className="div-about-faq">
-        <Col xs={2} md={3}>
+        <Col xs={2} md={5}>
+          <p className="faq-p">
+            <b>Gravity Colon Hydrotherapy Session (45-60 mins)</b>
+          </p>
+          <p className="faq-p">
+            <span className="third-section-about-icon">
+              <FaCircle />
+            </span>{' '}
+            One Session $130{' '}
+          </p>
+          <p className="faq-p">
+            <span className="third-section-about-icon">
+              <FaCircle />
+            </span>{' '}
+            Package of Three Sessions $350{' '}
+            <span className="cross-text">$390</span>
+          </p>
+          <p className="faq-p">
+            <span className="third-section-about-icon">
+              <FaCircle />
+            </span>{' '}
+            Package of Six Sessions $650{' '}
+            <span className="cross-text">$780</span>
+          </p>
+          <p className="faq-p">
+            <b>Super Fresh Start (New Clients Only)</b>
+          </p>
+          <p className="faq-p">
+            Come for your first colonic for regular price - get second one for
+            $75 if you come within two weeks
+          </p>
           <p className="coming">coming soon...</p>
         </Col>
-        <Col xs={10} md={9} className="faq" />
+        <Col xs={10} md={7} className="faq" />
       </Row>
     </Grid>
   </Layout>
