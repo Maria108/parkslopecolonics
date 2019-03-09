7992ff311ebf70f3b621437e56161007f851df79
Maria108 <maria.abash@gmail.com>
2019-03-09 15:07:45 -0500

    Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,6 @@ import { Grid, Row, Col } from 'react-bootstrap'
 
 import SecondSection from '../components/secondSection'
 import Testimonials from '../components/testimonials'
-// import Gallery from '../components/reactcar'
-
 import '../styles/bootstrap.scss'
 import '../styles/home.css'
 
