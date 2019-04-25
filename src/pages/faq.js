09198d39842e2a9c7484810390380fed273b39c0
Maria108 <maria.abash@gmail.com>
2019-04-25 19:54:16 -0400

    Changes

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -14,8 +14,9 @@ const Faq = () => (
     <Grid>
       <Row className="div-about-faq">
         <Col xs={2} md={5}>
-          <p className="faq-p">
-            <b>Gravity Colon Hydrotherapy Session (45-60 mins)</b>
+          <p className="prices">PRICES</p>
+          <p className="faq-price">
+            Gravity Colon Hydrotherapy Session (45-60 mins)
           </p>
           <p className="faq-p">
             <span className="third-section-about-icon">
@@ -37,16 +38,55 @@ const Faq = () => (
             Package of Six Sessions $650{' '}
             <span className="cross-text">$780</span>
           </p>
-          <p className="faq-p">
-            <b>Super Fresh Start (New Clients Only)</b>
+          <p className="faq-price-super">
+            Super Fresh Start (New Clients Only)
           </p>
           <p className="faq-p">
             Come for your first colonic for regular price - get second one for
             $75 if you come within two weeks
           </p>
-          <p className="coming">coming soon...</p>
         </Col>
         <Col xs={10} md={7} className="faq" />
+      </Row>
+      <Row>
+        <Col xs={12} md={12}>
+            <p className="third-section-home-header">FREQUENTLY ASKED QUESTIONS</p>
+        </Col>
+        <Col xs={12} md={12}>
+          <p className="question">How long is the session?</p>
+          <p className="answer">One session takes about 45 mins to an hour, after that you will finish up on a toilet releasing the remaining water</p>
+        </Col>
+        <Col xs={12} md={12}>
+          <p className="question">How many sessions do I need?</p>
+          <p className="answer">Frequency depends on your intentions and health concerns. If you are in good health and are interested in colonic for detox and optimum health, the recommendation for colon hygiene is every change of a season - 3-4 times a year. If you have any digestive issues, issues with skin, weight, inflammation, low energy or if you are doing a Cleanse right now - you most likely will need a series of colonics - that could be once a week for 3-6 weeks, every 2 weeks or every month depending on the issue. I will be able to judge when is the best next time for you to come after our session. As you feel the results, you will not need to do colonics that often. </p>
+        </Col>
+        <Col xs={12} md={12}>
+          <p className="question">Does it hurt?</p>
+          <p className="answer">No, Gravity method colonics are known for being gentle on the body. As your body releases matter and gas you may feel some cramping or pressure for a few seconds, that might be uncomfortable but not painful. Most clients feel comfortable and at ease during the treatment. I highly recommend to prepare for your session, following the dietary suggestions I offer will help you to avoid any discomfort.</p>
+        </Col>
+        <Col xs={12} md={12}>
+          <p className="question">Will colonic wash out all my good bacteria?</p>
+          <p className="answer">NO, there is no evidence for that and it is a myth. I have been getting colonics regularly for over 6 years and have been seeing clients who have been doing colonics for over 20 years and still have healthy digestion and regular bowel movements, that would be impossible if the bacteria in the colon was “washed out”. Your heathy bacteria can only survive in a proper, clean, healthy environment and colonic combined with a healthy diet plays a huge role in it. </p>
+        </Col>
+        <Col xs={12} md={12}>
+          <p className="question">How do I feel after?</p>
+          <p className="answer">You feel lighter and brighter! Feel free to go about you day. You should feel empty, light, relieved, rejuvenated, energized, happy. Increased sense of sight is very common. Lower back pain, headaches, puffyness often relieved after one treatment. If you feel less than great (sluggish, sleepy, dizzy, headaches, etc.), this is a sign that there is more work to do and you need to come back for a follow-up session within a week and be careful with food choices.</p>
+        </Col>
+        <Col xs={12} md={12}>
+          <p className="question">Who should not come in for colonic?</p>
+          <p className="answer">Most people can benefit from colonic, but there are some contradictions. You should consul with your doctor if you have conditions listed below:</p>
+            <p className="answer-points">Abdominal hernia</p>
+            <p className="answer-points">Acute Crohn’s disease, diverticulitis, abdominal pain</p>
+            <p className="answer-points">Severe liver, kidney or heart disease conditions</p>
+            <p className="answer-points">Epilepsy or psychological disorders</p>
+            <p className="answer-points">Pregnancy</p>
+            <p className="answer-points">Recent heat attack</p>
+            <p className="answer-points">Recent surgery (last 6 month)</p>
+            <p className="answer-points">Rectal or abdominal tumor</p>
+            <p className="answer-points">Severe hemorrhoids</p>
+            <p className="answer-points">Uncontrolled hypertension</p>
+            <p className="answer-points">Vascular aneurism</p>
+        </Col>
       </Row>
     </Grid>
   </Layout>
